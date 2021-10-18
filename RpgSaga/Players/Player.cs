83d1994e8f249bd45593a37c72b4b20cf3b5e278
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-18 13:10:12 +0300

    Added player classes

---
+++ RpgSaga/Players/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgSaga.Players
+{
+    public abstract class Player
+    {
+        public int Strength { get; set; }
+
+        public int Hp { get; set; }
+
+        public string Name { get; set; }
+    }
+}
