5d99e53796f4c0c577e0422596cbdf318f3fc4ff
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-18 16:53:38 +0300

    Created a layout for battle class

--- RpgSaga/Program.cs
+++ RpgSaga/Program.cs
@@ -1,13 +1,41 @@
 ﻿namespace CourseApp
 {
     using System;
+    using RpgSaga.Players;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello World");
+            Player p1 = new Mage { Name = "Oruma", Hp = 20, Strength = 3 };
+            Player p2 = new Warrior { Name = "Hrago", Hp = 30, Strength = 2 };
+            Battle(p1, p2);
+        }
+
+        public static void Battle(Player p1, Player p2)
+        {
+            while (p1.Hp > 0 && p2.Hp > 0)
+            {
+                p2.Hp -= p1.Strength;
+                Console.WriteLine(p1.GetType().Name + " " + p1.Name + $" deals {p1.Strength} damage to the " + p2.GetType().Name + " " + p2.Name + $"({p2.Hp}Hp)");
+
+                if (p1.Hp > 0 && p2.Hp > 0)
+                {
+                    p1.Hp -= p2.Strength;
+                    Console.WriteLine(p2.GetType().Name + " " + p2.Name + $" deals {p2.Strength} damage to the " + p1.GetType().Name + " " + p1.Name + $"({p1.Hp}Hp)");
+                }
+            }
+
+            Console.WriteLine();
+
+            if (p1.Hp > 0)
+            {
+                Console.WriteLine("The " + p1.GetType().Name + " " + p1.Name + " won");
+            }
+            else
+            {
+                Console.WriteLine("The " + p2.GetType().Name + " " + p2.Name + " won");
+            }
         }
     }
 }
