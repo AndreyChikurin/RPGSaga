69fb7b058ed415416fc85ac474ad20ff967dd2f4
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-18 17:58:41 +0300

    Fixed review points

---
+++ RpgSaga/Players/CreatePlayers.cs
@@ -0,0 +1,15 @@
+﻿namespace RpgSaga.Players
+{
+    public class CreatePlayers
+    {
+        public static Player NewPlayer1()
+        {
+            return new Mage { Name = "Oruma", Hp = 20, Strength = 3 };
+        }
+
+        public static Player NewPlayer2()
+        {
+            return new Warrior { Name = "Hrago", Hp = 30, Strength = 2 };
+        }
+    }
+}
