5a5c98f8b75519d2625e0dd3c7e90b12dd8a37aa
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-18 19:40:01 +0300

    Change PlayerMove method

--- RpgSaga/Combat/Game.cs
+++ RpgSaga/Combat/Game.cs
@@ -12,44 +12,35 @@
 
             while (p1.Hp > 0 && p2.Hp > 0)
             {
-                PlayerMove(p1, p2, true);
+                PlayerMove(p1, p2);
 
                 if (p2.Hp > 0)
                 {
-                    PlayerMove(p1, p2, false);
+                    PlayerMove(p2, p1);
                 }
             }
 
             Winner(p1, p2);
         }
 
-        public static void PlayerMove(Player p1, Player p2, bool firstPlayerMove)
+        public static void PlayerMove(Player attacker, Player defender)
         {
-            if (firstPlayerMove)
-            {
-                p2.Hp -= p1.Strength;
-                Console.WriteLine(p1.GetType().Name + " " + p1.Name + $" deals {p1.Strength} damage to the " + p2.GetType().Name + " " + p2.Name + $"({p2.Hp}Hp)");
-            }
-
-            if (!firstPlayerMove)
-            {
-                p1.Hp -= p2.Strength;
-                Console.WriteLine(p2.GetType().Name + " " + p2.Name + $" deals {p2.Strength} damage to the " + p1.GetType().Name + " " + p1.Name + $"({p1.Hp}Hp)");
-            }
+            defender.Hp -= attacker.Strength;
+            Console.WriteLine(attacker.GetType().Name + " " + attacker.Name + $" deals {attacker.Strength} damage to the " + defender.GetType().Name + " " + defender.Name + $"({defender.Hp}Hp)");
         }
 
         public static void Winner(Player p1, Player p2)
         {
             Console.WriteLine();
 
-            if (p1.Hp > 0)
+            if (p2.Hp > 0)
             {
-                Console.WriteLine("The " + p1.GetType().Name + " " + p1.Name + " won");
+                var a = p1;
+                p1 = p2;
+                p2 = a;
             }
-            else
-            {
-                Console.WriteLine("The " + p2.GetType().Name + " " + p2.Name + " won");
-            }
+
+            Console.WriteLine("The " + p1.GetType().Name + " " + p1.Name + " won");
         }
     }
 }
