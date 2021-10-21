8763dbe423215f200047ab6992123def049c8729
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-21 15:20:56 +0300

    Old commit

---
+++ RpgSaga/Interfaces/IEffect.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgSaga.Interfaces
+{
+    using RpgSaga.Players;
+
+    public interface IEffect
+    {
+        public int Duration { get; set; }
+
+        public void EffectAction(Player player);
+    }
+}
---
+++ RpgSaga/Interfaces/ISkill.cs
@@ -0,0 +1,9 @@
+﻿namespace RpgSaga.Interfaces
+{
+    using RpgSaga.Players;
+
+    public interface ISkill
+    {
+        public void SkillAction(Player soursePlayer, Player targetPlayer);
+    }
+}
