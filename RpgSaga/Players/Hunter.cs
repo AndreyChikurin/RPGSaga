becf598c278719ece2378a94ef5c6fe937a68225
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-21 16:02:35 +0300

    bug fix

--- RpgSaga/Players/Hunter.cs
+++ RpgSaga/Players/Hunter.cs
@@ -1,6 +1,13 @@
 ﻿namespace RpgSaga.Players
 {
+    using System.Collections.Generic;
+    using RpgSaga.Interfaces;
+
     public class Hunter : Player
     {
+        public Hunter(int strength, int hp, string name, List<ISkill> skills)
+            : base(strength, hp, name, skills)
+        {
+        }
     }
 }
--- RpgSaga/Program.cs
+++ RpgSaga/Program.cs
@@ -1,13 +1,9 @@
 ﻿namespace CourseApp
 {
-    using RpgSaga.Combat;
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var game = new Game();
-            game.RunGame();
         }
     }
 }
