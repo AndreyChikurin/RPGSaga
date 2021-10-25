98c63a24efd24ab9f939e5e57b7bfd8f3a5202b2
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-25 15:51:55 +0300

    Added enum2

--- RpgSaga/Game.cs
+++ RpgSaga/Game.cs
@@ -8,7 +8,7 @@
 
         public Game(LogType log)
         {
-            _logger = log == 0 ? new LoggerForConsole() : new LoggerForFile("C:\\Users\\andre\\Desktop\\RPG\\rpgsaganetcore\\RpgSaga\\bin\\Debug\\netcoreapp5\\");
+            _logger = log == LogType.LogConsole ? new LoggerForConsole() : new LoggerForFile("C:\\Users\\andre\\Desktop\\RPG\\rpgsaganetcore\\RpgSaga\\bin\\Debug\\netcoreapp5\\");
             _logger.Test();
         }
 
