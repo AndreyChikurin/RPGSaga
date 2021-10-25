6bc1140f2940a6dad4a81a1f182aacd8eda4ffa2
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-25 11:08:26 +0300

    deleted override(bug fix)

--- RpgSaga/Loggers/LoggerForConsole.cs
+++ RpgSaga/Loggers/LoggerForConsole.cs
@@ -5,22 +5,22 @@
 
     public class LoggerForConsole : ILogger
     {
-        public override void EffectLog(Player player, string effectName)
+        public void EffectLog(Player player, string effectName)
         {
             Console.WriteLine(player.GetType().Name + " " + player.Name + $" is under the effect {effectName}");
         }
 
-        public override void FightLog(Player attacker, Player defender)
+        public void FightLog(Player attacker, Player defender)
         {
             Console.WriteLine(attacker.GetType().Name + " " + attacker.Name + $" deals {attacker.Strength} damage to the " + defender.GetType().Name + " " + defender.Name + $"({defender.Hp}Hp)");
         }
 
-        public override void SkillLog(Player soursePlayer, Player targetPlayer, string skillName)
+        public void SkillLog(Player soursePlayer, Player targetPlayer, string skillName)
         {
             Console.WriteLine(soursePlayer.GetType().Name + " " + soursePlayer.Name + $" uses {skillName} on the " + targetPlayer.GetType().Name + " " + targetPlayer.Name);
         }
 
-        public override void WinnerLog(Player winner)
+        public void WinnerLog(Player winner)
         {
             Console.WriteLine("The " + winner.GetType().Name + " " + winner.Name + " won");
         }
--- RpgSaga/Loggers/LoggerForFile.cs
+++ RpgSaga/Loggers/LoggerForFile.cs
@@ -7,22 +7,22 @@
     {
         private readonly string path = @"c:\Users\andre\Desktop\MyTest.txt";
 
-        public override void EffectLog(Player player, string effectName)
+        public void EffectLog(Player player, string effectName)
         {
             File.WriteAllText(path, player.GetType().Name + " " + player.Name + $" is under the effect {effectName}");
         }
 
-        public override void FightLog(Player attacker, Player defender)
+        public void FightLog(Player attacker, Player defender)
         {
             File.WriteAllText(path, attacker.GetType().Name + " " + attacker.Name + $" deals {attacker.Strength} damage to the " + defender.GetType().Name + " " + defender.Name + $"({defender.Hp}Hp)");
         }
 
-        public override void SkillLog(Player soursePlayer, Player targetPlayer, string skillName)
+        public void SkillLog(Player soursePlayer, Player targetPlayer, string skillName)
         {
             File.WriteAllText(path, soursePlayer.GetType().Name + " " + soursePlayer.Name + $" uses {skillName} on the " + targetPlayer.GetType().Name + " " + targetPlayer.Name);
         }
 
-        public override void WinnerLog(Player winner)
+        public void WinnerLog(Player winner)
         {
             File.WriteAllText(path, "The " + winner.GetType().Name + " " + winner.Name + " won");
         }
