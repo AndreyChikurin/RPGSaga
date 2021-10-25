35b12258c2a687941d1b648e83e245bd535af441
Andrei Chikurin <andrey.chikurin19@gmail.com>
2021-10-25 17:22:43 +0300

    Added Tournament() CurrentWinner() Start() Filling() List<Player>

--- RpgSaga/Game.cs
+++ RpgSaga/Game.cs
@@ -1,20 +1,100 @@
 ﻿namespace RpgSaga
 {
+    using System;
+    using System.Collections.Generic;
+    using RpgSaga.Interfaces;
     using RpgSaga.Loggers;
+    using RpgSaga.Players;
+    using RpgSaga.Rounds;
+    using RpgSaga.Skills;
 
     public class Game
     {
+        private const int NumberOfPlayerTypes = 3;
+
         private ILogger _logger;
 
         public Game(LogType log)
         {
             _logger = log == LogType.LogConsole ? new LoggerForConsole() : new LoggerForFile(@"Logs");
+
+            Players = new List<Player>();
         }
 
         public enum LogType
         {
             LogConsole,
             LogFile,
+        }
+
+        public List<Player> Players { get; set; }
+
+        public Round CurrentRound { get; set; }
+
+        public int NumberOfPlayers { get; set; }
+
+        public void Filling()
+        {
+            Random random = new Random();
+
+            Players = new List<Player>(NumberOfPlayers);
+
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                switch (random.Next(0, NumberOfPlayerTypes))
+                {
+                    case 0:
+                        {
+                            Players.Add(new Hunter(
+                                random.Next(50, 70),
+                                random.Next(4, 7),
+                                PlayerNames.Hunter[random.Next(0, PlayerNames.Hunter.Length)],
+                                new List<ISkill> { new FireArrows(_logger) }));
+                            break;
+                        }
+
+                    case 1:
+                        {
+                            Players.Add(new Warrior(
+                                random.Next(50, 70),
+                                random.Next(4, 7),
+                                PlayerNames.Warrior[random.Next(0, PlayerNames.Warrior.Length)],
+                                new List<ISkill> { new MortalStrike(_logger) }));
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Players.Add(new Mage(
+                                random.Next(50, 70),
+                                random.Next(4, 7),
+                                PlayerNames.Mage[random.Next(0, PlayerNames.Mage.Length)],
+                                new List<ISkill> { new Freezing(_logger) }));
+                            break;
+                        }
+                }
+            }
+        }
+
+        public void Tournament()
+        {
+            while (Players.Count > 1)
+            {
+                CurrentRound = new Round(Players);
+                CurrentRound.Start();
+            }
+        }
+
+        public void CurrentWinner()
+        {
+            _logger.WinnerLog(Players[0]);
+        }
+
+        public void Start()
+        {
+            Filling();
+            Tournament();
+            CurrentWinner();
         }
     }
 }
---
+++ RpgSaga/Rounds/PlayerNames.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgSaga.Rounds
+{
+    public static class PlayerNames
+    {
+        public static readonly string[] Hunter = { "Rexar", "Heming", "Drenchel" };
+
+        public static readonly string[] Mage = { "Antonidas", "Invoker", "Oruma" };
+
+        public static readonly string[] Warrior = { "Grommash", "Garrosh", "Hrago" };
+    }
+}
