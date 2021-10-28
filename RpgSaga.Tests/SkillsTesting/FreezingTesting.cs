67db90d462f757775aa6af32d78f0e025d82f41b
Andrei Chikurin <andrei.chikurin@akvelon.com>
2021-10-28 12:59:50 +0300

    Added tests for skills

---
+++ RpgSaga.Tests/SkillsTesting/FreezingTesting.cs
@@ -0,0 +1,28 @@
+﻿namespace RpgSaga.Tests.SkillsTesting
+{
+    using System.Collections.Generic;
+    using RpgSaga.Effects;
+    using RpgSaga.Interfaces;
+    using RpgSaga.Loggers;
+    using RpgSaga.Players;
+    using RpgSaga.Skills;
+    using Xunit;
+
+    public class FreezingTesting
+    {
+        [Fact]
+        public void FreezingeTest()
+        {
+            ILogger testLogger = new LoggerForConsole();
+
+            Mage mage1 = new Mage(5, 60, "TestMage1", new List<ISkill> { new Freezing(testLogger) });
+            Mage mage2 = new Mage(5, 60, "TestMage2", new List<ISkill>());
+
+            List<IEffect> currentEffect = new List<IEffect>() { new SkipMove(testLogger, 1) };
+
+            mage1.Skills[0].SkillAction(mage1, mage2);
+
+            Assert.Equal(currentEffect[0], mage2.Effects[0]);
+        }
+    }
+}
