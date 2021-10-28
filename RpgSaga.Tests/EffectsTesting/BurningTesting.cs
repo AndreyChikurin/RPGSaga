61d2acfb7060ca1f18e8d4cfaa649920d47d21ab
Andrei Chikurin <andrei.chikurin@akvelon.com>
2021-10-28 14:53:59 +0300

    Added duration check for BurningTesting

--- RpgSaga.Tests/EffectsTesting/BurningTesting.cs
+++ RpgSaga.Tests/EffectsTesting/BurningTesting.cs
@@ -16,9 +16,13 @@
 
             Mage mage1 = new Mage(5, 60, "TestMage1", new List<ISkill>());
             mage1.Effects.Add(new Burning(testLogger));
+            int durationBefore = mage1.Effects[0].Duration;
 
             mage1.Effects[0].EffectAction(mage1);
 
+            int durationAfrer = mage1.Effects[0].Duration;
+
+            Assert.Equal(durationBefore, durationAfrer);
             Assert.Equal(mage1.MaxHp - 2, mage1.Hp);
         }
     }
