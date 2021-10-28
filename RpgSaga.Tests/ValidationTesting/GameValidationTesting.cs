467e74927f5862803cdff529d496487ffbcb010b
Andrei Chikurin <andrei.chikurin@akvelon.com>
2021-10-28 15:23:42 +0300

    Added GameValidation test

---
+++ RpgSaga.Tests/ValidationTesting/GameValidationTesting.cs
@@ -0,0 +1,40 @@
+﻿namespace RpgSaga.Tests.ValidationTesting
+{
+    using Xunit;
+
+    public class GameValidationTesting
+    {
+        [Fact]
+        public void GameValidationTest()
+        {
+            int numberOfMessages = 0;
+            var game = new Game();
+            game.Start("0", "2");
+
+            foreach (string errorMessage in game.ErrorMessages)
+            {
+                numberOfMessages++;
+            }
+
+            Assert.True(numberOfMessages == 0);
+
+            game.Start("test", "test");
+
+            foreach (string errorMessage in game.ErrorMessages)
+            {
+                numberOfMessages++;
+            }
+
+            Assert.True(numberOfMessages == 2);
+
+            game.Start("5", "5");
+
+            foreach (string errorMessage in game.ErrorMessages)
+            {
+                numberOfMessages++;
+            }
+
+            Assert.True(numberOfMessages == 4);
+        }
+    }
+}
